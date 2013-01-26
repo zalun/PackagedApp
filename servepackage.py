0b1de3103ca3e5f0afffa88fe8ff282a267d23dd
Piotr Zalewa <piotr@zalewa.info>
2013-01-26 07:20:02 +0100

    mini manifest mimetype added

--- servepackage.py
+++ servepackage.py
@@ -1,5 +1,6 @@
 import SimpleHTTPServer
 import SocketServer
 SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.webapp'] = 'application/x-web-app-manifest+json'
+SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.manifest'] = 'application/x-web-app-manifest+json'
 httpd = SocketServer.TCPServer(("", 3000), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.serve_forever()
